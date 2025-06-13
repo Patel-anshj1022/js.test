7a619023c44964b04ac888a84dafba167f552840
Ansh J Patel <patelanshj950@gmail.com>
2025-06-13 16:31:15 +0300

    add

---
+++ array.py
@@ -0,0 +1,27 @@
+fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"]
+print(fruits[1])  
+
+fruits.append("Kiwi")
+print(fruits)
+
+
+fruits.pop(0)
+print(fruits)
+
+
+fruits.sort()
+print(fruits)
+
+
+fruits.reverse()
+print(fruits)
+
+
+print("Mango" in fruits)  
+
+
+print(len(fruits))
+
+
+for fruit in fruits:
+    print(fruit.upper())
